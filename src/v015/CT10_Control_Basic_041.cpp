87ec8215f43d200d3486d6f12c7912b78b7d26d6
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-12-29 23:13:29 +0900

    Update CT10_Control_Basic_041.cpp

--- src/v015/CT10_Control_Basic_041.cpp
+++ src/v015/CT10_Control_Basic_041.cpp
@@ -13,49 +13,6 @@
 #include "CT10_Control_041.h"
 #include <DHT.h>
 
-// --------------------------------------------------
-// Dirty flags
-// --------------------------------------------------
-void CL_CT10_ControlManager::markDirty(const char* p_key) {
-	if (!p_key || p_key[0] == '\0')
-		return;
-
-	if (strcmp(p_key, "state") == 0) {
-		_dirtyState = true;
-	} else if (strcmp(p_key, "chart") == 0) {
-		_dirtyChart = true;
-	} else if (strcmp(p_key, "metrics") == 0) {
-		_dirtyMetrics = true;
-	} else if (strcmp(p_key, "summary") == 0) {
-		_dirtySummary = true;
-	} else {
-		CL_D10_Logger::log(EN_L10_LOG_DEBUG, "[CT10] markDirty: unknown key=%s", p_key);
-	}
-}
-
-bool CL_CT10_ControlManager::consumeDirtyState() {
-	bool v_ret = _dirtyState;
-	_dirtyState = false;
-	return v_ret;
-}
-
-bool CL_CT10_ControlManager::consumeDirtyMetrics() {
-	bool v_ret = _dirtyMetrics;
-	_dirtyMetrics = false;
-	return v_ret;
-}
-
-bool CL_CT10_ControlManager::consumeDirtyChart() {
-	bool v_ret = _dirtyChart;
-	_dirtyChart = false;
-	return v_ret;
-}
-
-bool CL_CT10_ControlManager::consumeDirtySummary() {
-	bool v_ret = _dirtySummary;
-	_dirtySummary = false;
-	return v_ret;
-}
 
 // --------------------------------------------------
 // override remain sec
@@ -303,17 +260,7 @@ bool CL_CT10_ControlManager::isMotionBlocked(const ST_A20_Motion_t& p_motionCfg)
 	return false;
 }
 
-// --------------------------------------------------
-// metrics dirty push
-// --------------------------------------------------
-void CL_CT10_ControlManager::maybePushMetricsDirty() {
-	unsigned long v_nowMs = millis();
-	if (v_nowMs - lastMetricsPushMs < S_METRICS_PUSH_INTERVAL_MS)
-		return;
 
-	lastMetricsPushMs = v_nowMs;
-	markDirty("metrics");
-}
 
 // --------------------------------------------------
 // preset/style name lookup (log 개선용)

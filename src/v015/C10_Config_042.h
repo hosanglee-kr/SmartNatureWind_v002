320eefc538eaab936fc7c3d77db5d2fd98fdeae3
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2026-01-06 20:43:24 +0900

    Update C10_Config_042.h

--- src/v015/C10_Config_042.h
+++ src/v015/C10_Config_042.h
@@ -74,7 +74,6 @@ extern ST_A20_ConfigRoot_t g_A20_config_root;
 // ------------------------------------------------------
 class CL_C10_ConfigManager {
   public:
-    CL_C10_ConfigManager();
 	// =====================================================
 	// 1. 전체 관리 (Load/Free/Save)
 	// =====================================================
@@ -184,7 +183,7 @@ class CL_C10_ConfigManager {
 	static bool _dirty_webPage;
 
     // ✅ Dirty 보호용 mux (클래스 소유)
-    static portMUX_TYPE _dirty_Mux;
+    static portMUX_TYPE s_dirtyMux;
 
 	// cfg_jsonFile.json 매핑
 	static ST_A20_cfg_jsonFile_t s_cfgJsonFileMap;

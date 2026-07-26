1d190beef390c75dc605ce2e65276caa0eae85f9
hslee <hslee>
2026-07-27 08:52:15 +0900

    update A20_Const_060.h

--- src/v016/A20_Const_060.h
+++ src/v016/A20_Const_060.h
@@ -76,8 +76,9 @@ class CL_M10_MotionLogic; // 전방 선언
 extern CL_M10_MotionLogic* g_M10_motionLogic;
 
 
+#include "A40_Com_Func_060.h"
 
 #include "A20_Com_Func_060.h"
 #include "A20_Com_FuncRst_060.h"
 
-#include "A40_Com_Func_060.h"
+
